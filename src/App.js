8758567e517521d32a190a21fd0238bd5bb99aed
Adarsh <adarsh1772000@gmail.com>
2021-08-29 00:32:08 +0530

    initial database calls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Teacher from './screen/teacher/Teacher';
 
 function App() {
 	const [loggedIn, setLoggedIn] = useState(false);
@@ -10,9 +11,11 @@ function App() {
 		});
 	}, []);
 
+	// return <Teacher />;
+
 	return (
 		<div className="App">
-			<div>Monk pathshala</div>
+			<Teacher />
 		</div>
 	);
 }
